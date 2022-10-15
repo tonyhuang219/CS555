4935d7df531fe405bc379dd316f4ca28abafaba7
bemin.shaker <beminshaker1@outlook.com>
2022-10-14 23:00:50 -0400

    added firebase functions

---
+++ ChoreApp/backend/firebase.ts
@@ -0,0 +1,113 @@
+import React from 'react'
+import { initializeApp } from 'firebase/app';
+import { getAuth, createUserWithEmailAndPassword, updateProfile, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { getFirestore, addDoc, collection, query, where, getDocs, DocumentSnapshot, getDoc, doc, updateDoc, arrayUnion, DocumentReference } from 'firebase/firestore';
+import Constants from 'expo-constants';
+import 'firebase/auth'
+import { getStorage, ref, uploadString } from "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: Constants.manifest?.extra?.firebaseApiKey,
+    authDomain: Constants.manifest?.extra?.firebaseAuthDomain,
+    projectId: Constants.manifest?.extra?.firebaseProjectId,
+    storageBucket: Constants.manifest?.extra?.firebaseStorageBucket,
+    messagingSenderId: Constants.manifest?.extra?.firebaseMessagingSenderId,
+    appId: Constants.manifest?.extra?.firebaseAppId,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const storage = getStorage(app);
+
+// AUTHENTICATION // ---------------------------------------------------------
+let user = auth.currentUser;
+
+export const signUpWithEmail = async (fName: string, lName: string, email: string, password: string) => {
+    try {
+        let result = await createUserWithEmailAndPassword(auth, email, password);
+        user = result.user;
+        await updateProfile(user, {
+            displayName: fName + ' ' + lName,
+        });
+        console.log(user);
+        await addNewUser(fName, lName, email);
+        return 'success'
+    } catch (e) {
+        console.log(e);
+       
+        return e;
+    }
+}
+
+export const logInWithEmail = async (email: string, password: string) => {
+    try {
+        let result = await signInWithEmailAndPassword(auth, email, password);
+        user = result.user;
+        return 'success'
+    } catch (e) {
+        console.log(e);
+        return e;
+    }
+}
+
+export const logOut = async () => {
+    try {
+        await signOut(auth);
+        user = auth.currentUser;
+        console.log(user)
+        return 'success'
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+// FIRESTORE // --------------------------------------------------------------
+const addNewUser = async (fName: string, lName: string, email: string) => {
+    try {
+        const userData = {
+            first_name: fName,
+            last_name: lName,
+            email: email,
+            cart: [],
+        }
+        const docRef = await addDoc(collection(firestore, "users", ), userData);
+        console.log(docRef.id);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export const getEmail = async () => {
+    let email = user?.email;
+    let email_one = "this";
+
+    if (email == null) {
+        email_one = 'null'
+    }
+    else if (email == undefined) {
+        email_one = 'undefined'
+    }
+    else {
+        email_one = email;
+    }
+
+    return email_one;
+}
+
+export const getFirstName = async () => {
+    let name = user?.displayName;
+    let name_one = "this";
+
+    if (name == null) {
+        name_one = 'null'
+    }
+    else if (name == undefined) {
+        name_one = 'undefined'
+    }
+    else {
+        name_one = name;
+    }
+
+    return name_one;
+}
