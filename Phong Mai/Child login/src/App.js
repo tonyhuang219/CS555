a9352bdea44770713edc01d2422f39b06b20ec82
Phong Mai <57877225+vuawind@users.noreply.github.com>
2022-10-14 21:13:07 -0400

    Add files via upload

---
+++ Phong Mai/Child login/src/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import "./styles.css"
+
+export default function LoginPageChild() {
+  return (
+    <div className="login-page-child flex-col-hstart-vstart clip-contents">
+      <div className="group-1064 flex-col-hcenter">
+        <div className="group-847 flex-row">
+          <div className="rectangle-11" />
+          <div className="rectangle-11" />
+          <div className="rectangle-11" />
+          <div className="rectangle-12" />
+        </div>
+        <div className="rectangle-10" />
+      </div>
+      <p className="txt-792 flex-hcenter">Login</p>
+    </div>
+  )
+}
